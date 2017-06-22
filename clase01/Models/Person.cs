52474e93c83af4c22196fe39a758e2f0a48f6032
James <j4mes18@gmail.com>
2017-06-22 09:13:06 -0500

    Creando carpeta Models y clase persona

---
+++ clase01/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase01.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } // usar prop y autocompleta la creación
+        public DateTime Birth { get; set; }
+    }
+}
